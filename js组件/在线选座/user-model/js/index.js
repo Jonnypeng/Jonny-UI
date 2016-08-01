17ae7ca22d1e1a6975dd8aab76aaef0078394f89
Jonny Peng <jonny.peng@qq.com>
2016-08-01 22:50:27 +0800

    完成大概的鼠标事件

--- js组件/在线选座/user-model/js/index.js
+++ js组件/在线选座/user-model/js/index.js
@@ -5,6 +5,7 @@ var cNum = 0;	//列位数
 var site = {start:{x:"",y:""},end:{x:"",y:""}};
 
 
+
 for(var i=0;i<=9;i++){
 	var row = document.createElement("ul");			//创建行
 	rNum = i;
@@ -21,15 +22,15 @@ for(var i=0;i<=9;i++){
 };
 
 var grid = document.getElementsByClassName("col");	
-var rGrid = document.getElementsByClassName("row");	
+var rGid = document.getElementsByClassName("row");	
 for(var k in grid){
 	grid[k].addEventListener("mousedown",start,false);
 };
 
 function start(){
 	console.log("start");
-	this.style.background = "red";
-	console.log(this.rNum,this.cNum);
+	this.style.background = "blue";
+	//console.log(this.rNum,this.cNum);
 	site.start.x = this.rNum;
 	site.start.y = this.cNum;
 	for(var l in grid){
@@ -38,8 +39,12 @@ function start(){
 
 };
 
-function proccess(){
-	console.log("proccess");	
+function proccess(event){
+var x = event.clientX;
+var y = event.clientY;
+	console.log(x,y);	
+	con.style.cursor = "se-resize";
+	this.style.background = "blue";
 	for(var m in grid){
 		grid[m].addEventListener("mouseup",end,false);
 	};
@@ -47,20 +52,20 @@ function proccess(){
 
 function end(){
 	console.log("end");	
-	this.style.background = "red";
+	this.style.background = "blue";
 	console.log(this.rNum,this.cNum);
 	site.end.x = this.rNum;
 	site.end.y = this.cNum;
-	console.log(site);
+	console.log(site.end.x);
 	for(var sx=site.start.x;sx<=site.end.x;sx++){
 		for(var sy=site.start.y;sy<=site.end.y;sy++){
 				rGid[sx].getElementsByTagName("li")[sy].style.background = "blue";					
 		};
 	};
+site = {start:{x:"",y:""},end:{x:"",y:""}};
+	con.style.cursor = "cell";
 	for(var n in grid){
-		grid[n].removeEventListener("mousedown",start,false);
 		grid[n].removeEventListener("mousemove",proccess,false);
-		grid[n].removeEventListener("mouseup",end,false);
 	};
 	};
 
